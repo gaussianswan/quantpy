783b122b55b1c01969c9d527e4b6cabacb29781b
gaussianswan <srerrie@gmail.com>
2023-03-22 23:01:03 -0400

    added a pdf fit function

--- quantpy/distributions.py
+++ quantpy/distributions.py
@@ -1,8 +1,16 @@
 import pandas as pd
+import numpy as np
 
+from enum import Enum
 from scipy.stats import norm, t, cauchy, laplace
 from dataclasses import dataclass
 
+class DistributionType(Enum):
+
+    NORMAL = 'normal'
+    T = 'T'
+    LAPLACE = 'laplace'
+    CAUCHY = 'cauchy'
 @dataclass
 class TFit:
 
@@ -54,6 +62,32 @@ def fit_laplace_distribution(data: pd.Series) -> LaplaceFit:
     laplace_fit = LaplaceFit(loc = fit[0], scale = fit[1])
 
     return laplace_fit
+
+def pdf_fit(data: pd.Series, num_points: int = 100, distribution: DistributionType = DistributionType.NORMAL) -> pd.Series:
+
+    support = np.linspace(data.min(), data.max(), num_points)
+
+    if distribution == DistributionType.NORMAL:
+        normal_fit = fit_normal_distribution(data = data)
+        pdf = norm.pdf(x = support, loc = normal_fit.loc, scale = normal_fit.scale)
+
+    elif distribution == DistributionType.CAUCHY:
+        cauchy_fit = fit_cauchy_distributoin(data = data)
+        pdf = norm.pdf(x = support, loc = cauchy_fit.loc, scale = cauchy_fit.scale)
+
+    elif distribution == DistributionType.LAPLACE:
+        laplace_fit = fit_laplace_distribution(data = data)
+        pdf = laplace.pdf(x = support, loc = laplace_fit.loc, scale = laplace_fit.scale)
+
+
+    elif distribution == DistributionType.T:
+        t_fit = fit_t_distribution(data = data)
+        pdf = t.pdf(x = support, df = t_fit.df, loc = t_fit.loc, scale = t_fit.scale)
+
+    return pd.Series(pdf, index = support)
+
+
+
 
 
 
